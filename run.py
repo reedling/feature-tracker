c2aeff913f4e891a0b58cf49456fff5159c0ad7a
Ty Carter <ty.carter5028@gmail.com>
2018-04-19 16:39:31 -0400

    favicon

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 import datetime
 from operator import attrgetter
+import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from flask_bootstrap import Bootstrap
 
 
@@ -33,7 +34,8 @@ fake_feature_reqs = [
     Feature('Charge for hand soap', 'They can\'t keep getting away with this.', 'Client A', 3, '04-06-2018', 'Billing'),
     Feature('TPS report', 'Thatd be great.', 'Client B', 1, '02-05-2018', 'Reports'),
     Feature('Simplify soap pipeline', 'It is a real hassle to get more hand soap when we run out.', 'Client A', 2, '05-06-2018', 'Policies'),
-    Feature('Make this website', 'OMG.', 'Client C', 1, '23-04-2018', 'Claims')
+    Feature('Make this website', 'OMG.', 'Client C', 1, '23-04-2018', 'Claims'),
+    Feature('The HTML/CSS is pretty much good to go!', 'Now we need to add KnockoutJS, SQLAlchemy, etc.', 'Client C', 2, '23-04-2018', 'Claims')
 ]
 
 
@@ -49,6 +51,13 @@ def index():
 @app.route('/new-request')
 def new_request():
     return render_template('new_request.html')
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico',
+                               mimetype='image/vnd.microsoft.icon')
 
 
 if __name__ == '__main__':
