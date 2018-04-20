06d73c22ebfe2664b78e6bde11992cddfa8f0bfc
Ty Carter <ty.carter5028@gmail.com>
2018-04-20 11:32:47 -0400

    make sure db.create_all() gets run -- may need to try before_first_request approach

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,6 @@ from app import db
 from run import flask_app as application
 
 
-if __name__ == "__main__":
-    with application.app_context():
-        db.create_all()
-        application.run()
+with application.app_context():
+    db.create_all()
+    application.run()
