c19f3573ba2b7cd0b62f39ea2344b47e03a9eb63
Ty Carter <ty.carter5028@gmail.com>
2018-04-20 11:57:27 -0400

    try getting DATABASE_URL for postgres connection in prod

--- config/prod.py
+++ config/prod.py
@@ -1,4 +1,6 @@
+import os
+
 DEBUG = True
 SECRET_KEY = 'secretTestingKeyThatHopefullyNoOneWillGuess'
-SQLALCHEMY_DATABASE_URI = 'postgres://utytvstquovlmq:ffe90bd0d5d2f964fd1d10bb20265050f2cbb4c05e3c1528821d78ffa389e8ca@ec2-23-23-180-121.compute-1.amazonaws.com:5432/dbalibajt9vd9l'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
