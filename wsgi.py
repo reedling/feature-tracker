e6a4c7a1b0b4fb40d46ccdca263714eb172a2e94
Ty Carter <ty.carter5028@gmail.com>
2018-04-20 11:58:52 -0400

    remove duplicate before_first_request definition

--- wsgi.py
+++ wsgi.py
@@ -2,11 +2,5 @@ from app import db
 from run import flask_app as application
 
 
-@application.before_first_request
-def initialize():
-    with application.app_context():
-        db.create_all()
-
-
 if __name__ == "__main__":
     application.run()
