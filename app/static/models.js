7730050e6ba60215d3f41446c632b2660b952805
Ty Carter <ty.carter5028@gmail.com>
2018-04-21 15:21:41 -0400

    validation with knockout

--- app/static/models.js
+++ app/static/models.js
@@ -4,20 +4,36 @@ clients = [
     'Client C'
 ];
 
+areas = [
+    'Policies',
+    'Billing',
+    'Claims',
+    'Reports'
+];
+
 function get_current_date() {
     var d = new Date();
     return (new Date(d.getTime() + (d.getTimezoneOffset()*60*1000*-1))).toISOString().split('T')[0];
 }
 
+ko.validation.rules.mustBeIn = {
+    validator: function (val, arr) {
+        return arr.indexOf(val) > -1;
+    },
+    message: 'A valid selection is required'
+};
+ko.validation.registerExtenders();
+
 function Feature(data) {
     var self = this;
+
     if (data === undefined) { // defaults
         self.done = ko.observable(false);
         self.counts = ko.observableArray([]);
-        self.title = ko.observable('');
+        self.title = ko.observable('').extend({ required: true, maxLength: 200 });
         self.desc = ko.observable('');
-        self.client = ko.observable('');
-        self.priority = ko.observable('');
+        self.client = ko.observable('').extend({ required: true, mustBeIn: clients });
+        self.priority = ko.observable('').extend({ required: true });
         self.disablePriority = ko.computed(function() {
             disable = clients.indexOf(self.client()) === -1;
             if (disable) self.priority('');
@@ -38,8 +54,8 @@ function Feature(data) {
                 return possible;
             }
         }, self);
-        self.target_date = ko.observable(get_current_date());
-        self.area = ko.observable('');
+        self.target_date = ko.observable(get_current_date()).extend({ required: true, date: true});
+        self.area = ko.observable('').extend({ required: true, mustBeIn: areas });
         $.getJSON('/request-counts', function(counts) {
             var mappedCounts = $.map(counts, function(count, client) {
                 return { client: client, count: count };
@@ -47,6 +63,20 @@ function Feature(data) {
             self.counts(mappedCounts);
             self.done(true);
         });
+
+        self.checkValidity = function() {
+            var valid = self.title.isValid() &&
+                self.client.isValid() &&
+                self.priority.isValid() &&
+                self.target_date.isValid() &&
+                self.area.isValid();
+            if (!valid) {
+                ko.validation.group(self).showAllMessages(true);
+                return false;
+            } else {
+                return true;
+            }
+        };
     } else { // passed values
         self.title = data.title;
         self.desc = data.desc;
