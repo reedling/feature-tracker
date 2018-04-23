5f116bc785761f7ade61f5fad7803dc4389d9ee0
Ty Carter <ty.carter5028@gmail.com>
2018-04-22 21:31:28 -0400

    Update configs

--- config/dev.py
+++ config/dev.py
@@ -1,4 +1,6 @@
+import os
+
 DEBUG = True
 SECRET_KEY = 'secretTestingKeyThatHopefullyNoOneWillGuess'
-SQLALCHEMY_DATABASE_URI = 'postgresql://tycarter@localhost/tycarter'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
--- config/prod.py
+++ config/prod.py
@@ -1,6 +1,6 @@
 import os
 
-DEBUG = True
+DEBUG = False
 SECRET_KEY = 'secretTestingKeyThatHopefullyNoOneWillGuess'
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
